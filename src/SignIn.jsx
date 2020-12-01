2a8e1045e649251154e8c6fe664169fa83e664e2
Christopher W. Butler <cbutler.msstate@gmail.com>
2020-12-01 10:33:32 -0600

    Added sign in form

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, Text, TextInput, View } from 'react-native';
+import { useForm, Controller } from "react-hook-form";
 import BGScreen from './BackgroundScreen';
 import { primary_color } from './styles';
 
 export default function SignIn() {
+  const { control, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log('signIn', data);
+
   return (
     <BGScreen>
-      <View style={{ marginTop: 75, marginBottom: 20 }}>
+      <View style={{ marginTop: 100, marginBottom: 60 }}>
         <Text style={{ fontSize: 30, fontWeight: 'bold', color: 'white', textAlign: 'center' }}>
           Welcome Back
         </Text>
@@ -14,6 +18,91 @@ export default function SignIn() {
           Sign in to access Black Family Wealth Community
         </Text>
       </View>
+
+      <View style={{ flexDirection: 'column', padding: 10, flex: 1 }}>
+        <Input
+          control={control}
+          errors={errors}
+          name="email"
+          placeholder="Email*"
+          rules={{ required: true }}
+          errorText="This is required"
+        />
+  
+        <Input
+          control={control}
+          errors={errors}
+          name="password"
+          placeholder="Password*"
+          rules={{ required: true }}
+          errorText="This is required"
+        />
+      </View>
+
+      <View style={{ flexDirection: 'column', padding: 10, }}>
+        <Pressable
+          style={{
+            backgroundColor: primary_color,
+            borderRadius: 18,
+            padding: 12,
+            alignItems: 'center',
+            marginBottom: 8
+          }}
+          onPress={handleSubmit(onSubmit)}
+        >
+          <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>
+            Sign In
+          </Text>
+        </Pressable>
+
+        <Text style={{ fontSize: 12, color: 'gray', textAlign: 'center' }}>
+          By proceeding you also agree to the Terms of Service and Privacy Policy
+        </Text>
+      </View>
     </BGScreen>
+  );
+}
+
+function Input(props) {
+  const hasError = props.errors && props.errors[props.name];
+  const isPassword = props.name === 'password';
+
+  return (
+     <View style={{ flexDirection: 'column', padding: 4 }}>
+      <Controller
+        control={props.control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            style={{
+              backgroundColor: 'white',
+              paddingLeft: 8,
+              paddingRight: 8,
+              paddingTop: 16,
+              paddingBottom: 16,
+              borderRadius: 4,
+              fontSize: 16,
+              borderColor: (hasError) ? 'red' : undefined,
+              borderWidth: (hasError) ? 2 : undefined
+            }}
+            placeholder={props.placeholder}
+            placeholderTextColor="gray"
+            onBlur={onBlur}
+            onChangeText={text => onChange(text)}
+            value={value}
+            secureTextEntry={isPassword}
+            textContentType={(isPassword) ? 'password' : 'emailAddress'}
+            autoCompleteType={(isPassword) ? 'password' : 'email'}
+          />
+        )}
+        name={props.name}
+        rules={props.rules}
+        defaultValue={props.defaultValue || ''}
+      />
+      {hasError && (
+        <Text style={{ color: 'red', padding: 4 }}>
+          * {props.errorText}
+        </Text>
+      )}
+     </View>
   );
 }
