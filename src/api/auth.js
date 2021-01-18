22161046b41778f6d4d781742b3f7ab508ea17b7
Christopher W. Butler <cbutler.msstate@gmail.com>
2021-01-18 15:04:01 -0500

    Fixed errors with spaces in email

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,8 @@
-import { API, Auth } from 'aws-amplify';
+import { API, Auth, Storage } from 'aws-amplify';
 import * as mutations from "../../graphql/mutations";
 
 export async function signUp(input) {
-  const email = input.email.toLowerCase();
+  const email = input.email.toLowerCase().trim();
   const { user } = await Auth.signUp({
     username: email,
     password: input.password,
@@ -18,6 +18,7 @@ export async function signUp(input) {
 }
 
 export async function createUser(input) {
+  input.email && input.email.trim();
   const { data } = await API.graphql({
       query: mutations.createUser,
       variables: { input },
@@ -27,11 +28,18 @@ export async function createUser(input) {
 }
 
 export async function confirmUser({ email, code }) {
-  const data = await Auth.confirmSignUp(email.toLowerCase(), code);
+  const data = await Auth.confirmSignUp(email.toLowerCase().trim(), code);
   return data;
 }
 
 export async function updateUserAttributes({ user, attributes }) {
   const data = await Auth.updateUserAttributes(user, attributes);
   return data;
+}
+
+export async function saveProfileImage({ user, image }) {
+  console.log(image)
+  //await Storage.put(`user/avatar/${user.attributes.sub}`, image);
+  //const data = await Auth.updateUserAttributes(user, attributes);
+  return;
 }
