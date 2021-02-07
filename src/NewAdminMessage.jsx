ecc5ef4a10020dc791ffae3d0cdc169861bb4f3b
Christopher W. Butler <cbutler.msstate@gmail.com>
2021-02-07 12:45:46 -0500

    Fixed keyboard hiding toolbar

--- src/NewAdminMessage.jsx
+++ src/NewAdminMessage.jsx
@@ -1,5 +1,5 @@
 import React,{ useContext, useRef } from 'react';
-import { Pressable, Text, TextInput, View, ScrollView } from 'react-native';
+import { Pressable, Text, TextInput, View, ScrollView, Platform, KeyboardAvoidingView } from 'react-native';
 import { useForm, Controller } from "react-hook-form";
 import { RichEditor, RichToolbar, actions } from 'react-native-pell-rich-editor'
 import BGScreen from './BackgroundScreen';
@@ -22,74 +22,80 @@ export default function NewAdminMessage() {
 
   return (
     <BGScreen>
-      <View style={{ flex: 1, backgroundColor: 'white' }}>
-        <View style={{ flexDirection: 'row', padding: 10, borderColor: 'lightgrey', borderBottomWidth: 1 }}>
-          <Text>To:</Text>
-          <Text style={{ fontWeight: 'bold' }}> Admin</Text>
-        </View>
-        <View style={{ flexDirection: 'row', padding: 10, borderColor: 'lightgrey', borderBottomWidth: 1 }}>
-          <Text>From:</Text>
-          <Text style={{ fontWeight: 'bold' }}> {user.attributes.email}</Text>
-        </View>
-        <View style={{ flexDirection: 'row', padding: 10, borderColor: 'lightgrey', borderBottomWidth: 1 }}>
-          <Text>Subject:</Text>
-          <Controller
-            name="subject"
-            control={control}
-            defaultValue=""
-            render={({ onChange, onBlur, value }) => (
-              <TextInput
-                onBlur={onBlur}
-                onChangeText={text => onChange(text)}
-                value={value}
-                style={{ flex: 1, marginLeft: 6 }}
-              />
-            )}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
+        keyboardVerticalOffset={90}
+      >
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+          <View style={{ flexDirection: 'row', padding: 10, borderColor: 'lightgrey', borderBottomWidth: 1 }}>
+            <Text>To:</Text>
+            <Text style={{ fontWeight: 'bold' }}> Admin</Text>
+          </View>
+          <View style={{ flexDirection: 'row', padding: 10, borderColor: 'lightgrey', borderBottomWidth: 1 }}>
+            <Text>From:</Text>
+            <Text style={{ fontWeight: 'bold' }}> {user.attributes.email}</Text>
+          </View>
+          <View style={{ flexDirection: 'row', padding: 10, borderColor: 'lightgrey', borderBottomWidth: 1 }}>
+            <Text>Subject:</Text>
+            <Controller
+              name="subject"
+              control={control}
+              defaultValue=""
+              render={({ onChange, onBlur, value }) => (
+                <TextInput
+                  onBlur={onBlur}
+                  onChangeText={text => onChange(text)}
+                  value={value}
+                  style={{ flex: 1, marginLeft: 6 }}
+                />
+              )}
+            />
+          </View>
+          <ScrollView style={{ flex: 1, padding: 10 }}>
+            <Controller
+              name="message"
+              control={control}
+              defaultValue=""
+              render={({ onChange, onBlur, value }) => (
+                <RichEditor
+                  ref={editor}
+                  onBlur={onBlur}
+                  onChange={text => onChange(text)}
+                  placeholder="Type Message..."
+                />
+              )}
+            />
+          </ScrollView>
+          <RichToolbar
+            editor={editor}
+            onPressAddImage={onPressAddImage}
+            actions={[
+              actions.keyboard,
+              actions.insertImage,
+              actions.setBold,
+              actions.setItalic,
+              actions.setUnderline,
+              actions.setStrikethrough,
+              actions.insertBulletsList,
+              actions.insertOrderedList,
+              actions.checkboxList,
+              actions.insertLink,
+              actions.insertVideo,
+              actions.undo,
+              actions.redo
+            ]}
           />
         </View>
-        <ScrollView style={{ flex: 1, padding: 10 }}>
-          <Controller
-            name="message"
-            control={control}
-            defaultValue=""
-            render={({ onChange, onBlur, value }) => (
-              <RichEditor
-                ref={editor}
-                onBlur={onBlur}
-                onChange={text => onChange(text)}
-                placeholder="Type Message..."
-              />
-            )}
-          />
-        </ScrollView>
-        <RichToolbar
-          editor={editor}
-          onPressAddImage={onPressAddImage}
-          actions={[
-            actions.keyboard,
-            actions.insertImage,
-            actions.setBold,
-            actions.setItalic,
-            actions.setUnderline,
-            actions.setStrikethrough,
-            actions.insertBulletsList,
-            actions.insertOrderedList,
-            actions.checkboxList,
-            actions.insertLink,
-            actions.insertVideo,
-            actions.undo,
-            actions.redo
-          ]}
-        />
-      </View>
 
-      <View style={{ padding: 10 }}>
-        <Pressable style={{ backgroundColor: primary_color, padding: 14, borderRadius: 4 }}>
-          <Text style={{ color: 'white', fontSize: 16, textAlign: 'center' }}>
-            Send message
-          </Text>
-        </Pressable>
-      </View>
+        <View style={{ padding: 10 }}>
+          <Pressable style={{ backgroundColor: primary_color, padding: 14, borderRadius: 4 }}>
+            <Text style={{ color: 'white', fontSize: 16, textAlign: 'center' }}>
+              Send message
+            </Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
     </BGScreen>
   );
 }
